a3bfc6b090f3e2af2ba652846dffe240ddf22db5
kaia <111490006+kaia-dan@users.noreply.github.com>
2022-12-14 10:22:55 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// function btnActive()  {
+//   const target = 
+//   document.getElementById('.allWrap .change_c');
+//   target.disabled = false;
+// }
+
+// function btnDisabled()  {
+//   const target = 
+//   document.getElementById('.allWrap .change_c');
+//   target.disabled = true;
+// }
